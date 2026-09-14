623f3f23a7b5575a636b25343289f27f881c82b9
Worldwideditz <belloabubakar2404@gmail.com>
2026-09-14 22:00:52 +0100

    Create Product.kt

---
+++ data/Product.kt
@@ -0,0 +1,11 @@
+package com.alubarika.store.data
+
+data class Product(
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val price: Double = 0.0,
+    val imageUrl: String = "",
+    val category: String = "",
+    val stock: Int = 0
+)
